042132dec37cc95424b33eae159187e4aee899dc
mk2023 <77135480+mk2023@users.noreply.github.com>
2026-03-07 21:21:00 -0500

    Fixing the error

--- app/auth/callback/route.tsx
+++ app/auth/callback/route.tsx
@@ -1,32 +1,32 @@
 import { NextResponse } from "next/server";
-import type {NextRequest} from "next/server";
+import type { NextRequest } from "next/server";
 import { createServerClient } from "@supabase/ssr";
-import {cookies} from "next/headers";
 
-export async function GET(request: NextRequest) {
-  const url = new URL(request.url);
+export async function GET(req: NextRequest) {
+  const url = req.nextUrl.clone();
   const code = url.searchParams.get("code");
 
-  const response = NextResponse.redirect(`${url.origin}/protected`);
+  if (code) {
+    const res = NextResponse.redirect(new URL("/admin", url.origin));
 
-  if (!code) return response;
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll: () => req.cookies.getAll(),
+          setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
+            cookiesToSet.forEach(({ name, value, options }) => {
+              res.cookies.set(name, value, options);
+            });
+          },
+        },
+      }
+    );
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => {
-            response.cookies.set(name, value, options);
-          });
-        },
-      },
-    }
-  );
-  await supabase.auth.exchangeCodeForSession(code);
-  return response;
+    await supabase.auth.exchangeCodeForSession(code);
+    return res;
+  }
+
+  return NextResponse.redirect(new URL("/", url.origin));
 }
--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export async function middleware(req: NextRequest) {
     {
       cookies: {
         getAll: () => req.cookies.getAll(),
-        setAll: (cookiesToSet) => {
+        setAll: (cookiesToSet: { name: string; value: string; options?: any }[]) => {
           cookiesToSet.forEach(({ name, value, options }) => {
             res.cookies.set(name, value, options);
           });
